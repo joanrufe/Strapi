555b19e8cee2e3a024eea13750b775397a5b63a1
José Antonio Ruiz <jose.ruiz@eu.jll.com>
2022-12-07 13:37:55 +0100

    adding generic content

---
+++ src/api/generic-content/routes/generic-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * generic-content router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::generic-content.generic-content');
---
+++ src/api/generic-content/services/generic-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * generic-content service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::generic-content.generic-content');
